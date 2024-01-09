6c145971c20262c68fd0f2e0e3f28920f63bfba8
Russell <russellfebrisa@gmail.com>
2024-01-09 21:30:49 +0700

    Routing added

--- src/App.js
+++ src/App.js
@@ -3,36 +3,34 @@ import Header from './Header';
 import HomePage from './AgenHome';
 import Listing from './DetailedProfile';
 import Form from './Apply';
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import Payment from './Payment';
 import Pembantu from './pembantu';
 import Supir from './supir';
-import BabySitter from './babysitter'
+import BabySitter from './babysitter';
 import PenajakHewan from "./penajak-hewan";
 import PerawatLansia from "./perawat-lansia";
 import TukangKebun from './tukang-kebun';
 import SearchResult from './SeachBar';
-
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Router>
- <Header/>
+        <Header />
         <div className='content'>
           <Routes>
-            <Route path='/Home' element={<HomePage />} />
-            <Route path='/Listing' element={<Listing />} />
-            <Route path ='/hiring' element= {<Form/>}/>
-              <Route path='/pembantu' element={<Pembantu />} />
-              <Route path='/supir' element={<Supir/>} />
-              <Route path='/baby-sitter' element={<BabySitter/>} />
-              <Route path='/perawat-lansia' element={<PerawatLansia />} />
-              <Route path='/penajak-hewan' element={<PenajakHewan />} />
-              <Route path='/tukang-kebun' element={<TukangKebun />} />
-              <Route path = 'SearchResult' element={<SearchResult/>}/>
-              <Route path = '/Listing/payment' element={<Payment/>}/>
-        
+            <Route path='Home' element={<HomePage />} />
+            <Route path='Listing' element={<Listing />} />
+            <Route path='hiring' element={<Form />} />
+            <Route path='pembantu' element={<Pembantu />} />
+            <Route path='supir' element={<Supir />} />
+            <Route path='baby-sitter' element={<BabySitter />} />
+            <Route path='perawat-lansia' element={<PerawatLansia />} />
+            <Route path='penajak-hewan' element={<PenajakHewan />} />
+            <Route path='tukang-kebun' element={<TukangKebun />} />
+            <Route path='SearchResult' element={<SearchResult />} />
+            <Route path='Listing/payment' element={<Payment />} />
           </Routes>
         </div>
       </Router>
@@ -41,4 +39,3 @@ function App() {
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <HashRouter>
     <React.StrictMode>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <App />
     </React.StrictMode>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
